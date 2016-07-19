19a6830fba4613a0b346122d3189a98df186f8d2
mariaxtina <mchristinahess@gmail.com>
2016-07-18 19:25:50 -0700

    Add different elements to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Message from './Message.jsx';
+// import MessageList from './MessageList.jsx';
 
-class App extends Component {
+const App = React.createClass ({
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+      <nav><h1>Chatty</h1></nav>
+      <Message/>
+      <ChatBar/>
+      </div>
+
     );
   }
-}
+});
+
 export default App;

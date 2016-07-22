90d30f376c7da72247adeacc651752c95ffc3ac6
mariaxtina <mchristinahess@gmail.com>
2016-07-22 12:33:44 -0700

    Add render styles for message list

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,1 +1,20 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+const MessageList = React.createClass({
+  render() {
+    console.log("Rendering <MessageList/>");
+    return (
+      <div className="message-list">
+      {
+        this.props.messages.map(function (message, key) {
+          return <Message key={key} message={message} id={message.id} />;
+        })
+      }
+      </div>
+    );
+  }
+});
+
+export default MessageList;
 
